7c0904e17fdbba3f370f57b8918d06388ce4c791
Jesper Bruun Hansen <jesper@mentalact.dk>
2016-11-01 23:03:53 +0100

    login and request

--- js/app.js
+++ js/app.js
@@ -1,14 +1,51 @@
 $(document).ready(function () {
 
-  $.ajax({
-    url: "http://localhost:8080/api/book",
-    method: "GET",
-    success: function (foo, bar, lorem){
-      console.log(foo, bar, lorem)
-    },
-    error:function(foo, bar){
-      console.log(foo, bar)
-    }
+  $("#loginButton").on("click", function(e){
+    e.preventDefault();
+
+    var email = $("#inputEmail").val();
+    var pw = $("#inputPassword").val();
+
+    SDK.login(email, pw);
+
   });
 
 });
+
+var SDK = {
+
+  request:function(options, cb){
+    $.ajax({
+      url: "http://localhost:3000/api" + options.url,
+      method: options.method,
+      headers: options.headers,
+      contentType: "application/json",
+      dataType: "json",
+      data: JSON.stringify(options.data),
+      success:function(data, status, xhr){
+        cb(null, data, status, xhr);
+      },
+      error:function(xhr, status, errorThrown){
+        cb({xhr:xhr, status:status, error:errorThrown});
+      }
+    });
+  },
+
+  login: function(username, password){
+    this.request({
+      data:{
+        username:username,
+        password:password
+      },
+      url: "/staffs/login",
+      method: "POST"
+    }, function(err, data){
+
+      if(err) return console.log(err);
+
+      console.log(data);
+
+    });
+  }
+
+};
