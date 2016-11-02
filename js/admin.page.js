b1138de1b3f18cfd2460890436b9b87878ed3980
Jesper Bruun Hansen <jesper@mentalact.dk>
2016-11-02 09:24:47 +0100

    create admin-panel

---
+++ js/admin.page.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function () {
+
+  //Fires on page-load
+  SDK.Book.getAll(function(err, data){
+    if(err) throw err;
+
+    function printAuthors(authors){
+      return authors.map(function(author){
+        return author.firstName + " " + author.lastName;
+      }).join(", ");
+    }
+
+    var $booksTableBody = $("#booksTableBody");
+    data.forEach(function (book, i) {
+
+      $booksTableBody.append(
+        "<tr>" +
+        "<td>" + book.title + "</td>" +
+        "<td>" + book.subtitle  + "</td>" +
+        "<td>" + printAuthors(book.authors) + "</td>" +
+        "<td>" + book.publisher.name + "</td>" +
+        "<td>Kr. " + book.price + ",-</td>" +
+        "</tr>");
+    });
+
+  });
+
+});
