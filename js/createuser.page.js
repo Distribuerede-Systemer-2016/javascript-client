da85433ffdc7cea5c3099832787a5da3331ba7ef
Christoffer <weisleder@gmail.com>
2017-01-23 18:39:04 +0100

    Admin book

--- js/createuser.page.js
+++ js/createuser.page.js
@@ -1,39 +1,26 @@
 /**
  * Created by Christoffer on 13-12-2016.
  */
-$(document).ready(function () {
+$("#addNewUserButton").on("click", function() {
 
-    $("#createUserButton").on("click", function(e){
-        e.preventDefault();
+    var user = {
+        username: $("#newUserUserName").val(),
+        password: $("#newUserPassword").val(),
+        phonenumber: +$("#newUserPhonenumber").val(),
+        address: $("#newUserAdress").val(),
+        email: $("#newUserEmail").val(),
+        mobilepay: +$("#mobilepay").prop("checked"),
+        cash: +$("#cash").prop("checked"),
+        transfer: +$("#transfer").prop("checked")
+    };
+    console.log(user);
 
-        var username = $("inputUsername").val();
-        var pw = $("#inputPassword").val();
-        var email = $("#inputEmail").val();
-        var address = $("inputaddress").val();
-        var phonenumber = $("inputPhonenumber").val();
-        var mobilepay = $("inputMobilepay").val();
-        var cash = $("inputCash").val();
-        var transfer = $("inputTransfer").val();
+    SDK.User.create(user, function(err, data) {
+        if (err) throw JSON.stringify(err);
+        console.log(user.username);
 
-        SDK.Book.create(username, email, pw, address, phonenumber, mobilepay, cash, transfer, function(err, data){
-
-            if(err) {
-            return $("#createUserForm").find(".form-group").addClass("has-error");
-            }
-
-            //user created succesfull
-            $("#createUserForm").find(".form-group").addClass("has-success");
-
-
-
-
-            $("#logOutLink").on("click", function(){
-                SDK.logOut();
-                window.location.href = "index.html";
-            });
-
-        });
-
+        /*alert("Tillykke, du har nu opretet en bruger");*/
+        window.location.href = "index.html";
     });
 
 });
@@ -41,56 +28,4 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-
-
-/*
-$(document).ready(function () {
-    SDK.User.create(function(err, data){
-        if(err) throw err;
-
-        function createuser(username, password, email, phonenumber, address, mobilepay, cash, transfer, cb) {
-            var username = $("#username").val();
-            var password = $("#password").val();
-            var email = $ ("#email").val();
-            var phonenumber = $ ("#phonenumber").val();
-            var address = $ ("#adress").val();
-            var mobilepay = $ ("#mobilepay").val();
-            var cash = $ ("#cash").val();
-            var transfer = $ ("#transfer").val();
-
-
-            $.ajax({
-                type: "POST",
-                dataType: "json",
-                xhrFields: { withCredentials: true },
-                data: JSON.stringify({
-                    "username" : username,
-                    "password" : password,
-                    "email" : email,
-                    "phonenumber" : phonenumber,
-                    "address" : address,
-                    "mobilepay" : mobilepay,
-                    "cash" : cash,
-                    "transfer" : transfer
-                }),
-                success: function(data) {
-                    alert(JSON.stringify(data));
-                },
-                error: function(data) {
-                    alert(JSON.stringify(data));
-                }
-            });
-        }
-
-
-            $("#logOutLink").on("click", function(){
-            SDK.logOut();
-            window.location.href = "index.html";
-        });
-    })
-});
-    */
+ */
