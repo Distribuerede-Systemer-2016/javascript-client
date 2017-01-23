d5b325b22d4a51d4d89d7c0be4cdd909f89cea72
Christoffer <weisleder@gmail.com>
2017-01-23 18:50:37 +0100

    Login update

--- js/login.page.js
+++ js/login.page.js
@@ -1,36 +1,39 @@
+    $(document).ready(function () {
 
-$(document).ready(function () {
+        $("#loginButton").on("click", function(e){
+            e.preventDefault();
 
-  $("#loginButton").on("click", function(e){
-    e.preventDefault();
+            var username = $("#username").val();
+            var password = $("#password").val();
 
-    var email = $("#inputEmail").val();
-    var pw = $("#inputPassword").val();
+            SDK.Identification.login(username, password, function(err, data){
 
-    SDK.login(email, pw, function(err, data){
 
-      //On wrong credentials
-      if(err) {
-        return $("#loginForm").find(".form-group").addClass("has-error");
-      }
+                //On wrong credentials
+                if(err) {
+                    return $("#loginForm").find(".form-group").addClass("has-error");
 
-      //Login OK!
-      $("#loginForm").find(".form-group").addClass("has-success");
+                }
 
-        if (data.type === 1){
-            window.location.href = "admin.html";
-        } else {
-            window.location.href = "ads.html";
-        }
+                //Login OK!
+                if (data.type == 1){
+                    $("#loginForm").find(".form-group").addClass("has-success");
+                    window.location.href = "adminpage.html";
+                }
+                else{
+                    $("#loginForm").find(".form-group").addClass("has-success");
+                    window.location.href = "userpage.html";
 
+                }
+
+
+            });
+        });
 
         $("#logOutLink").on("click", function(){
-            SDK.logOut();
+            SDK.Identification.logOut();
             window.location.href = "index.html";
+
         });
 
     });
-
-  });
-
-});
